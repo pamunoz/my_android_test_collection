c6cc60531456588de6cf983de2fa2b50558a1cf2
Pablo Munoz <pfariasmunoz@gmail.com>
2016-11-18 11:15:24 -0300

    Check Network Connectivity status

--- udacity/ud843/QuakeReport/app/src/main/java/com/pfariasmunoz/quakereport/MainActivity.java
+++ udacity/ud843/QuakeReport/app/src/main/java/com/pfariasmunoz/quakereport/MainActivity.java
@@ -15,6 +15,9 @@
  */
 package com.pfariasmunoz.quakereport;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -66,16 +69,23 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(earthquakeAdapter);
 
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
-
-
-
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+            // fetch data
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            loadingIndicator.setVisibility(View.GONE);
+            // display error
+            emptyListView.setText(R.string.no_internet_connection);
+        }
     }
 
     /**
