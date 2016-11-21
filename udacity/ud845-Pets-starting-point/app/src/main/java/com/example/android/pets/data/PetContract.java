89bf81ac7ff89c81871ccd4e39b30d7640425d64
Pablo Munoz <pfariasmunoz@gmail.com>
2016-11-21 16:17:27 -0300

    Add PetContract for db

---
+++ udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,32 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Pablo Farias on 21-11-16.
+ */
+
+public final class PetContract {
+    public static final String TAT = "Pets";
+
+    private PetContract(){}
+
+    public static class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * values for gender
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+    }
+}
