cdfecc2b3599eab1e19980aa5b7f8c6a1a57c6b9
Pablo Munoz <pfariasmunoz@gmail.com>
2016-11-27 19:31:32 -0300

    Add ContentProvider getType() Method

--- udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/data/PetContract.java
+++ udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -35,6 +36,18 @@ public final class PetContract {
      * Each entry in the table represents a single pet.
      */
     public static class PetEntry implements BaseColumns {
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
 
         /**
          * inside each of the Entry classes in the contract,
