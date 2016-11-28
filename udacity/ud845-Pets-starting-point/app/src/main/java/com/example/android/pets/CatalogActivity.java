e6062e9093c02d0f555dfe3a23830dbe78286d9d
Pablo Munoz <pfariasmunoz@gmail.com>
2016-11-28 16:34:19 -0300

    Use EditorActivity For Editing Pets Too

--- udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.pets;
 
 import android.app.LoaderManager;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.Intent;
@@ -28,6 +29,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.android.pets.data.PetContract.PetEntry;
@@ -69,6 +71,28 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         // There is no pet data yet (untill the loader finishes) so pass in null for the Cursor.
         mCursorAdapter = new PetCursorAdapter(this, null);
         petListView.setAdapter(mCursorAdapter);
+
+        // Set up item click listener
+        petListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new Intent to go to {@link EditorActivity}
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+
+                // Form the content URI that represent the specific pet that was clicked on,
+                // by appending the "id" (passed as imput to this method) onto the
+                // {@link PetEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.android.pets/pets/2"
+                // if the pet with ID 2 was clicked on.
+                Uri currentPetUri = ContentUris.withAppendedId(PetEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentPetUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                startActivity(intent);
+            }
+        });
 
         // kick off the loader
         getLoaderManager().initLoader(PET_LOADER, null, this);
--- udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/EditorActivity.java
+++ udacity/ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.pets;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
@@ -60,6 +61,21 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        // Examine the intent that was used to launch this activity
+        // in order to figure out if we're creating a new pet or editing an existing one.
+        Intent intent = getIntent();
+        Uri currentPetUri = intent.getData();
+
+        // if the intent DOES NOT containg a pet content URI, then we know that we are
+        // creating a new pet.
+        if (currentPetUri == null) {
+            // This is a new pet, so change the app bar to say "Add a pet"
+            setTitle(getString(R.string.edidtor_activity_title_add_pet));
+        } else {
+            // Otherwise this is an existing pet, so change the app bar to say "Edit pet"
+            setTitle(getString(R.string.edidtor_activity_title_edit_pet));
+        }
 
         // Find all relevant views that we will need to read user input from
         mNameEditText = (EditText) findViewById(R.id.edit_pet_name);
