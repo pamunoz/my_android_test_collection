89108676b534139aa24588de3b29e32012dd6212
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-03 15:45:56 -0300

    Add ProgressBar and ErrorMessage

--- udacity/ud851-Exercises-student/Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ udacity/ud851-Exercises-student/Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     // DONE (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
     // DONE (3) Override onPostExecute to display the results in the TextView
 
-    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+    private class GithubQueryTask extends AsyncTask<URL, Void, String> {
         @Override
         protected String doInBackground(URL... urls) {
             URL searchUrl = urls[0];
