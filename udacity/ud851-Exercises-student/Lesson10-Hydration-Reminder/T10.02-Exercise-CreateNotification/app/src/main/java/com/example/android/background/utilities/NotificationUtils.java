5998f156301e89559fc2254aec1f2506206e4661
Pablo Munoz <pfariasmunoz@gmail.com>
2016-12-16 16:38:51 -0300

    Add method for PendingIntent and Bitmap

--- udacity/ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.02-Exercise-CreateNotification/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ udacity/ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.02-Exercise-CreateNotification/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -1,9 +1,20 @@
 package com.example.android.background.utilities;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.example.android.background.MainActivity;
+import com.example.android.background.R;
+
 /**
  * Utility class for creating hydration notifications
  */
 public class NotificationUtils {
+    public static final int WATER_REMINDER_PENDING_INTENT_ID = 3517;
 
     // TODO (7) Create a method called remindUserBecauseCharging which takes a Context.
     // This method will create a notification for charging. It might be helpful
@@ -28,25 +39,38 @@ public class NotificationUtils {
 
 
 
-    // TODO (1) Create a helper method called contentIntent with a single parameter for a Context. It
+    // DONE (1) Create a helper method called contentIntent with a single parameter for a Context. It
     // should return a PendingIntent. This method will create the pending intent which will trigger when
     // the notification is pressed. This pending intent should open up the MainActivity.
-        // TODO (2) Create an intent that opens up the MainActivity
-        // TODO (3) Create a PendingIntent using getActivity that:
-            // - Take the context passed in as a parameter
-            // - Takes an unique integer ID for the pending intent (you can create a constant for
-            //   this integer above
-            // - Takes the intent to open the MainActivity you just created; this is what is triggered
-            //   when the notification is triggered
-            // - Has the flag FLAG_UPDATE_CURRENT, so that if the intent is created again, keep the
-            // intent but update the data
+    private PendingIntent contentIntent(Context context) {
+        // DONE (2) Create an intent that opens up the MainActivity
+        Intent intent = new Intent(context, MainActivity.class);
+        // DONE (3) Create a PendingIntent using getActivity that:
+        // - Take the context passed in as a parameter
+        // - Takes an unique integer ID for the pending intent (you can create a constant for
+        //   this integer above
+        // - Takes the intent to open the MainActivity you just created; this is what is triggered
+        //   when the notification is triggered
+        // - Has the flag FLAG_UPDATE_CURRENT, so that if the intent is created again, keep the
+        // intent but update the data
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                WATER_REMINDER_PENDING_INTENT_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return pendingIntent;
+    }
 
 
-    // TODO (4) Create a helper method called largeIcon which takes in a Context as a parameter and
+
+    // DONE (4) Create a helper method called largeIcon which takes in a Context as a parameter and
     // returns a Bitmap. This method is necessary to decode a bitmap needed for the notification.
-        // TODO (5) Get a Resources object from the context.
-        // TODO (6) Create and return a bitmap using BitmapFactory.decodeResource, passing in the
+
+    private Bitmap largeIcon(Context context) {
+        // DONE (5) Get a Resources object from the context.
+        Resources resources = context.getResources();
+        // DONE (6) Create and return a bitmap using BitmapFactory.decodeResource, passing in the
         // resources object and R.drawable.ic_local_drink_black_24px
+        Bitmap largeIcon = BitmapFactory.decodeResource(resources, R.drawable.ic_local_drink_black_24px);
+        return largeIcon;
+    }
 
 
 }
