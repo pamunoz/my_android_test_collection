d7ff20bbd90d5a097aada40ddf90c6daa83b0de2
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-04 19:31:29 -0300

    Add smarter syncing

---
+++ udacity/ud851-Sunshine-student/S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+// DONE (5) Create a new class called SunshineSyncIntentService that extends IntentService
+public class SunshineSyncIntentService extends IntentService {
+//  DONE (6) Create a constructor that calls super and passes the name of this class
+
+    public SunshineSyncIntentService() {
+        super("SunshineSyncIntentService");
+    }
+    //  DONE (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SunshineSyncTask.syncWeather(this);
+    }
+}
