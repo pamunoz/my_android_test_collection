fc371e9e7f8bad6d36b6dabeb530153e7acd5de1
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-24 20:08:53 -0300

    Add layout for the list items

---
+++ NewsApp/app/src/main/java/com/pfariasmunoz/newsapp/QueryUtils.java
@@ -0,0 +1,11 @@
+package com.pfariasmunoz.newsapp;
+
+/**
+ * Created by Pablo Farias on 24-01-17.
+ */
+
+/**
+ * Helper methods related to requesting and receiving earthquake data from USGS.
+ */
+public class QueryUtils {
+}
