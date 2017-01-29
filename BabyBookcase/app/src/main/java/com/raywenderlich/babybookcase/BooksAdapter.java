341ffd7ca2cd624f3a8ab0dece99b3df7fd7e861
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-29 15:13:55 -0300

    Add the BaseAdapter class

---
+++ BabyBookcase/app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
@@ -0,0 +1,46 @@
+package com.raywenderlich.babybookcase;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class BooksAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private final Book[] books;
+
+    // 1
+    public BooksAdapter(Context context, Book[] books) {
+        this.mContext = context;
+        this.books = books;
+    }
+
+    // 2
+    @Override
+    public int getCount() {
+        return books.length;
+    }
+
+    // 3
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // 4
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    // 5
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView dummyTextView = new TextView(mContext);
+        dummyTextView.setText(String.valueOf(position));
+        return dummyTextView;
+    }
+
+}
