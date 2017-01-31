4c84e9647954df8e934d5362b705848ed82187e6
Pablo Munoz <pfariasmunoz@gmail.com>
2017-01-31 18:03:07 -0300

    Add toggle favorite book

--- BabyBookcase/app/src/main/java/com/raywenderlich/babybookcase/MainActivity.java
+++ BabyBookcase/app/src/main/java/com/raywenderlich/babybookcase/MainActivity.java
@@ -24,42 +24,56 @@ package com.raywenderlich.babybookcase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    GridView gridView = (GridView)findViewById(R.id.gridview);
-    BooksAdapter booksAdapter = new BooksAdapter(this, books);
-    gridView.setAdapter(booksAdapter);
-
-  }
-
   private Book[] books = {
-    new Book(R.string.abc_an_amazing_alphabet_book, R.string.dr_seuss, R.drawable.abc,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/abc.jpg"),
-    new Book(R.string.are_you_my_mother, R.string.dr_seuss, R.drawable.areyoumymother,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/areyoumymother.jpg"),
-    new Book(R.string.where_is_babys_belly_button, R.string.karen_katz, R.drawable.whereisbabysbellybutton,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/whereisbabysbellybutton.jpg"),
-    new Book(R.string.on_the_night_you_were_born, R.string.nancy_tillman, R.drawable.onthenightyouwereborn,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/onthenightyouwereborn.jpg"),
-    new Book(R.string.hand_hand_fingers_thumb, R.string.dr_seuss, R.drawable.handhandfingersthumb,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/handhandfingersthumb.jpg"),
-    new Book(R.string.the_very_hungry_caterpillar, R.string.eric_carle, R.drawable.theveryhungrycaterpillar,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/theveryhungrycaterpillar.jpg"),
-    new Book(R.string.the_going_to_bed_book, R.string.sandra_boynton, R.drawable.thegoingtobedbook,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/thegoingtobedbook.jpg"),
-    new Book(R.string.oh_baby_go_baby, R.string.dr_seuss, R.drawable.ohbabygobaby,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/ohbabygobaby.jpg"),
-    new Book(R.string.the_tooth_book, R.string.dr_seuss, R.drawable.thetoothbook,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/thetoothbook.jpg"),
-    new Book(R.string.one_fish_two_fish_red_fish_blue_fish, R.string.dr_seuss, R.drawable.onefish,
-      "http://www.raywenderlich.com/wp-content/uploads/2016/03/onefish.jpg")
+      new Book(R.string.abc_an_amazing_alphabet_book, R.string.dr_seuss, R.drawable.abc,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/abc.jpg"),
+      new Book(R.string.are_you_my_mother, R.string.dr_seuss, R.drawable.areyoumymother,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/areyoumymother.jpg"),
+      new Book(R.string.where_is_babys_belly_button, R.string.karen_katz, R.drawable.whereisbabysbellybutton,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/whereisbabysbellybutton.jpg"),
+      new Book(R.string.on_the_night_you_were_born, R.string.nancy_tillman, R.drawable.onthenightyouwereborn,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/onthenightyouwereborn.jpg"),
+      new Book(R.string.hand_hand_fingers_thumb, R.string.dr_seuss, R.drawable.handhandfingersthumb,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/handhandfingersthumb.jpg"),
+      new Book(R.string.the_very_hungry_caterpillar, R.string.eric_carle, R.drawable.theveryhungrycaterpillar,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/theveryhungrycaterpillar.jpg"),
+      new Book(R.string.the_going_to_bed_book, R.string.sandra_boynton, R.drawable.thegoingtobedbook,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/thegoingtobedbook.jpg"),
+      new Book(R.string.oh_baby_go_baby, R.string.dr_seuss, R.drawable.ohbabygobaby,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/ohbabygobaby.jpg"),
+      new Book(R.string.the_tooth_book, R.string.dr_seuss, R.drawable.thetoothbook,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/thetoothbook.jpg"),
+      new Book(R.string.one_fish_two_fish_red_fish_blue_fish, R.string.dr_seuss, R.drawable.onefish,
+        "http://www.raywenderlich.com/wp-content/uploads/2016/03/onefish.jpg")
   };
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
+
+          GridView gridView = (GridView)findViewById(R.id.gridview);
+          final BooksAdapter booksAdapter = new BooksAdapter(this, books);
+          gridView.setAdapter(booksAdapter);
+
+
+          gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+              @Override
+              public void onItemClick(AdapterView parent, View view, int position, long id) {
+                  Book book = books[position];
+                  book.toggleFavorite();
+
+                  // This tells the GridView to redraw itself
+                  // in turn calling your BooksAdapter's getView method again for each cell
+                  booksAdapter.notifyDataSetChanged();
+              }
+          });
+      }
 
 }
