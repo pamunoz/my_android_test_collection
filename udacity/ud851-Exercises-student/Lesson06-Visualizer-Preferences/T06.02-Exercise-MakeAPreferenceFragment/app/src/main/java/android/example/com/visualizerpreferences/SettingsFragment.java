26b599fc99f6f26e03281974d8a3e159229aa63a
Pablo Munoz <pfariasmunoz@gmail.com>
2017-03-15 08:12:46 -0300

    Add Setting Fragment and xml resource

---
+++ udacity/ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,10 @@
+package android.example.com.visualizerpreferences;
+
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by Pablo Farias on 15-03-17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+}
