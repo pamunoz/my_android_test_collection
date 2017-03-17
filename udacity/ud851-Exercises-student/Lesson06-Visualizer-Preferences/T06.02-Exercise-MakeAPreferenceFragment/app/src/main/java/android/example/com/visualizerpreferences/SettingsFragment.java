5e6de645410f80ce7aeda9a6fd0ea7d2ff60f5a5
Pablo Munoz <pfariasmunoz@gmail.com>
2017-03-16 22:35:15 -0300

    Add resources for the preference fragment

--- udacity/ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ udacity/ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -1,5 +1,6 @@
 package android.example.com.visualizerpreferences;
 
+import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 /**
@@ -7,4 +8,9 @@ import android.support.v7.preference.PreferenceFragmentCompat;
  */
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
 }
