265213e14f59c6518f6c1b66c923ec2946272bc4
Pablo Munoz <pfariasmunoz@gmail.com>
2017-03-28 21:58:52 -0300

    Add exercise sunshine displaydayforecast

--- udacity/ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ udacity/ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,32 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private TextView mTextViewDisplayWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        // DONE (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+
+
+        mTextViewDisplayWeather = (TextView) findViewById(R.id.tv_weather_display);
+        if (intent != null) {
+            if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String weatherString = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mTextViewDisplayWeather.setText(weatherString);
+            }
+        }
+
     }
 }
