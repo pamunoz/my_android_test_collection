33f649b54f360a6a0f59658a1b2405ee63b04bc5
Pablo Munoz <pfariasmunoz@gmail.com>
2017-03-30 22:14:54 -0300

    Add tabs to the toolbar

--- android-design-library-master/1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
+++ android-design-library-master/1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
         // Adding Toolbar to Main screen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
     }
 
     @Override
