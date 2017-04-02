a3a8a1adc2bb17cad8935635a3136bf572f3eb55
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-02 20:55:10 -0300

    Refactor Sistema de Ventas 2

--- SistemaDeVentas/app/src/main/java/com/pfariasmunoz/sistemadeventas/smartlock/SplashFragment.java
+++ SistemaDeVentas/app/src/main/java/com/pfariasmunoz/sistemadeventas/smartlock/SplashFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.pfariasmunoz.sistemadeventas.R;
+
 public class SplashFragment extends Fragment {
 
     @Override
