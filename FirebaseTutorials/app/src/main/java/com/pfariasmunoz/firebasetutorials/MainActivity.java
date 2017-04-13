ffd909ea9a178adebbda27b7e76dd427470beac1
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-13 18:02:55 -0300

    Add basic read and write to db

--- FirebaseTutorials/app/src/main/java/com/pfariasmunoz/firebasetutorials/MainActivity.java
+++ FirebaseTutorials/app/src/main/java/com/pfariasmunoz/firebasetutorials/MainActivity.java
@@ -2,12 +2,72 @@ package com.pfariasmunoz.firebasetutorials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mMessagesReference;
+    private Button mAddMessageButton;
+    private TextView mMessageTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mMessagesReference = mDatabase.getReference("message");
+
+        addMessage("Hola como estan");
+
+        mMessageTextView = (TextView) findViewById(R.id.tv_message);
+        mAddMessageButton = (Button) findViewById(R.id.set_message);
+        mAddMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMessage();
+            }
+        });
+
+
+
+    }
+
+    private void addMessage(String message) {
+        // Write a message to the database
+        mMessagesReference.setValue(message);
+    }
+
+    private void getMessage() {
+        // Read from the database
+        mMessagesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                mMessageTextView.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
