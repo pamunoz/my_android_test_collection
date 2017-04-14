2fd1b44917ca757d3dd351138e4e7cdb3731665e
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-14 15:04:39 -0300

    Add listener for getting list of objects

--- FirebaseTutorials/app/src/main/java/com/pfariasmunoz/firebasetutorials/MainActivity.java
+++ FirebaseTutorials/app/src/main/java/com/pfariasmunoz/firebasetutorials/MainActivity.java
@@ -14,12 +14,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText mMessageEditText;
     private Button mSaveMessageButton;
     private EditText mEmailEditText;
+
+    private ArrayList<String> mNamesList = new ArrayList<>();
 
 
     @Override
@@ -56,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         mSetMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getMessage();
+                getUsers();
             }
         });
 
@@ -71,8 +75,9 @@ public class MainActivity extends AppCompatActivity {
                 dataMap.put("Name", name);
                 dataMap.put("Email", email);
 
-                if (!TextUtils.isEmpty(name)) {
-                    addNewUser(dataMap);
+                if (!TextUtils.isEmpty(name) || !TextUtils.isEmpty(email)) {
+                    //addNewUser(dataMap);
+                    addUser(dataMap);
                 } else {
                     Toast.makeText(MainActivity.this, "Failed to add User", Toast.LENGTH_SHORT).show();
                 }
@@ -80,12 +85,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
 
-    private void addUserUser(HashMap<String, String> data) {
+    private void addUser(HashMap<String, String> data) {
         // Write a message to the database
-        mMessagesReference.setValue(data);
+        mDatabaseReference.setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Saved User", Toast.LENGTH_LONG).show();
+                } else  {
+                    Toast.makeText(MainActivity.this, "Ho, the humanity!! we failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void addNewUser(HashMap<String, String> data) {
@@ -105,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void getMessage() {
         // Read from the database
-        mMessagesReference.addValueEventListener(new ValueEventListener() {
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
@@ -121,6 +134,39 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    private void getUsers() {
+        final StringBuilder builder = new StringBuilder();
+
+        mDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue().toString().trim();
+                builder.append(value + "\n");
+                mMessageTextView.append(builder.toString());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
