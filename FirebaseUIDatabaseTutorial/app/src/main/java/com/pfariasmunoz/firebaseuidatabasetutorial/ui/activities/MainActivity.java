cd1e15659b53bb852d3cd405eafc069037db5d79
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-14 18:21:50 -0300

    Add fragments files

--- FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/activities/MainActivity.java
+++ FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pfariasmunoz.firebaseuidatabasetutorial;
+package com.pfariasmunoz.firebaseuidatabasetutorial.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.pfariasmunoz.firebaseuidatabasetutorial.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
