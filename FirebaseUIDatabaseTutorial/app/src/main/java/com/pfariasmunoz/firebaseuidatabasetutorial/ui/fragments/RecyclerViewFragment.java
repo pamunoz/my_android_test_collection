549f21fb4239a9b8a95736dbd73ef15a5b693517
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-14 19:06:59 -0300

    Add layout for post items

--- FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/fragments/RecyclerViewFragment.java
+++ FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/fragments/RecyclerViewFragment.java
@@ -3,16 +3,23 @@ package com.pfariasmunoz.firebaseuidatabasetutorial.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.pfariasmunoz.firebaseuidatabasetutorial.R;
+import com.pfariasmunoz.firebaseuidatabasetutorial.data.models.Post;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecyclerViewFragment extends Fragment {
+
+    private RecyclerView mPostRecyclerView;
+    private FirebaseRecyclerAdapter<Post, PostViewHolder> mPostPostViewHolder;
+
 
 
     public RecyclerViewFragment() {
@@ -27,4 +34,11 @@ public class RecyclerViewFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_recycler_view, container, false);
     }
 
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
+
+
+        public PostViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
