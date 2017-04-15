c9db12068b8fdbae72dd7a001b7c3dd81c41b334
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-15 12:59:16 -0300

    Add layout for the indexrecyclerview and reference it

--- FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/fragments/IndexRecyclerViewFragment.java
+++ FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/fragments/IndexRecyclerViewFragment.java
@@ -3,16 +3,29 @@ package com.pfariasmunoz.firebaseuidatabasetutorial.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseIndexRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
 import com.pfariasmunoz.firebaseuidatabasetutorial.R;
+import com.pfariasmunoz.firebaseuidatabasetutorial.data.models.Post;
+import com.pfariasmunoz.firebaseuidatabasetutorial.utils.Constants;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class IndexRecyclerViewFragment extends Fragment {
+
+    private View mRootView;
+    private RecyclerView mIndexRecyclerView;
+    private FirebaseIndexRecyclerAdapter<Post, RecyclerViewFragment.PostViewHolder> mPostIndexAdapter;
+    private DatabaseReference mPostIndexDbReference;
+    private String[] mEmailList = Constants.EMAIL_LIST;
 
 
     public IndexRecyclerViewFragment() {
@@ -24,7 +37,14 @@ public class IndexRecyclerViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_index_recycler_view, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_index_recycler_view, container, false);
+        initializeView();
+        return mRootView;
+    }
+
+    private void initializeView() {
+        int viewId = R.id.indexedRecyclerView;
+        mIndexRecyclerView = (RecyclerView) mRootView.findViewById(viewId);
     }
 
 }
--- FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/fragments/ListViewFragment.java
+++ FirebaseUIDatabaseTutorial/app/src/main/java/com/pfariasmunoz/firebaseuidatabasetutorial/ui/fragments/ListViewFragment.java
@@ -73,5 +73,5 @@ public class ListViewFragment extends Fragment {
             }
         };
     }
-    
+
 }
