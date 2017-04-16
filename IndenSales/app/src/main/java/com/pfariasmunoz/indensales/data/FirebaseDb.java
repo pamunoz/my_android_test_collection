9540d0eac2912d3baedc47d5294dda25a2e5900d
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-16 12:00:55 -0300

    Add Firebase utility for db references

--- IndenSales/app/src/main/java/com/pfariasmunoz/indensales/data/FirebaseDb.java
+++ IndenSales/app/src/main/java/com/pfariasmunoz/indensales/data/FirebaseDb.java
@@ -1,8 +1,28 @@
 package com.pfariasmunoz.indensales.data;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Pablo Farias on 16-04-17.
  */
 
 public class FirebaseDb {
+    
+    public static final FirebaseDatabase sIndenDbRef = FirebaseDatabase.getInstance();
+    public static final DatabaseReference sClientsRef = sIndenDbRef.getReference(DbContract.CLIENTS_ND);
+    public static final DatabaseReference sArticlesRef = sIndenDbRef.getReference(DbContract.ARTICLES_ND);
+    public static final DatabaseReference sClientAdressRef = sIndenDbRef.getReference(DbContract.CLIENT_ADRES_ND);
+    public static final DatabaseReference sArticlesSalesRef = sIndenDbRef.getReference(DbContract.ARTICLES_SALES_ND);
+    public static final DatabaseReference sSalesRef = sIndenDbRef.getReference(DbContract.SALES_ND);
+
+
+    public static String getUid(DatabaseReference databaseReference) {
+        String url = databaseReference.push().toString();
+        if (url.contains("/")) {
+            return url.substring(url.lastIndexOf("/"), url.length());
+        } else {
+            return "Nothing found";
+        }
+    }
 }
