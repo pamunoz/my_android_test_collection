4433ea0cb047d2aa8346d9f720297c24e0f98c13
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-21 09:28:47 -0300

    Add add sales activity

---
+++ IndenSales/app/src/main/java/com/pfariasmunoz/indensales/ui/activities/AddSaleActivity.java
@@ -0,0 +1,15 @@
+package com.pfariasmunoz.indensales.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pfariasmunoz.indensales.R;
+
+public class AddSaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sale);
+    }
+}
