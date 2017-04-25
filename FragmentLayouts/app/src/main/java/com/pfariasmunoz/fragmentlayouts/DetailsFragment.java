a5b7891d7f94e437ca22d14793b7abaa08dc6f91
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-25 16:45:14 -0300

    Finish Details fragment

--- FragmentLayouts/app/src/main/java/com/pfariasmunoz/fragmentlayouts/DetailsFragment.java
+++ FragmentLayouts/app/src/main/java/com/pfariasmunoz/fragmentlayouts/DetailsFragment.java
@@ -1,13 +1,40 @@
 package com.pfariasmunoz.fragmentlayouts;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 /**
  * Created by Pablo Farias on 25-04-17.
  */
 
 public class DetailsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ScrollView scrollView = new ScrollView(getActivity());
+        TextView heroDataTextView = new TextView(getActivity());
+        // a way to specify dimmention using specific units
+        int padding = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 4,
+                getActivity().getResources().getDisplayMetrics());
+        // apply the padding to the textview in all its sides
+        heroDataTextView.setPadding(padding, padding, padding, padding);
+        // add the textview to the scroolview
+        scrollView.addView(heroDataTextView);
+        // set the text from the helpler class for the hero
+        // in the specific index selected
+        heroDataTextView.setText(SuperHeroInfo.HISTORY[getShowIndex()]);
+        // return the scrollview that contains the textview
+        return scrollView;
+    }
 
     public static DetailsFragment getInstance(int index) {
         DetailsFragment f = new DetailsFragment();
@@ -19,6 +46,6 @@ public class DetailsFragment extends Fragment {
     }
 
     public int getShowIndex() {
-        return 0;
+        return getArguments().getInt("index", 0);
     }
 }
