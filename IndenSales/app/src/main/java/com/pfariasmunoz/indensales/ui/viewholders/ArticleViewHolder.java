0cb464a79342c023956303f7c99bd6e244d6ac16
Pablo Munoz <pfariasmunoz@gmail.com>
2017-04-26 15:40:34 -0300

    Add vies to articles viewholders

--- IndenSales/app/src/main/java/com/pfariasmunoz/indensales/ui/viewholders/ArticleViewHolder.java
+++ IndenSales/app/src/main/java/com/pfariasmunoz/indensales/ui/viewholders/ArticleViewHolder.java
@@ -1,7 +1,9 @@
 package com.pfariasmunoz.indensales.ui.viewholders;
 
+import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.pfariasmunoz.indensales.R;
@@ -12,17 +14,37 @@ import com.pfariasmunoz.indensales.data.models.Article;
  */
 
 public class ArticleViewHolder extends RecyclerView.ViewHolder {
-    TextView mDescriptionTextView;
-    TextView mPriceTextView;
+    private TextView mDescriptionTextView;
+    private TextView mPriceTextView;
+    private TextView mAmountTextView;
+    private ImageButton mAddButton;
+    private ImageButton mSubstractButton;
+
 
     public ArticleViewHolder(View itemView) {
         super(itemView);
         mDescriptionTextView = (TextView) itemView.findViewById(R.id.tv_article_description);
         mPriceTextView = (TextView) itemView.findViewById(R.id.tv_article_price);
+        mAmountTextView = (TextView) itemView.findViewById(R.id.tv_article_amount);
+        mAddButton = (ImageButton) itemView.findViewById(R.id.imb_up_arrow);
+        mSubstractButton = (ImageButton) itemView.findViewById(R.id.imb_down_arrow);
     }
 
     public void setTextOnViews(Article article) {
         mDescriptionTextView.setText(article.getDescripcion());
         mPriceTextView.setText(article.getPrecio());
+    }
+
+    public void setAmount(int amount) {
+        String stringAmount = String.valueOf(amount);
+        mAmountTextView.setText(stringAmount);
+    }
+
+    public ImageButton getAddButton() {
+        return mAddButton;
+    }
+
+    public ImageButton getSubstractButton() {
+        return mSubstractButton;
     }
 }
