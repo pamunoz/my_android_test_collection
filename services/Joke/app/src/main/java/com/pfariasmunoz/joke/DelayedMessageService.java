0a53cb08356061b45531354d34c76276559c7222
Pablo Munoz <pfariasmunoz@gmail.com>
2017-09-30 14:53:07 -0300

    Finish project for services

--- services/Joke/app/src/main/java/com/pfariasmunoz/joke/DelayedMessageService.java
+++ services/Joke/app/src/main/java/com/pfariasmunoz/joke/DelayedMessageService.java
@@ -1,11 +1,21 @@
 package com.pfariasmunoz.joke;
 
 import android.app.IntentService;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Toast;
+
 
 
 /**
@@ -18,14 +28,9 @@ public class DelayedMessageService extends IntentService {
 
     // Use a constant to pass a message from the actiity to the service
     public static final String EXTRA_MESSAGE = "message";
-    private Handler mHandler;
+    // this is for identify the notification
+    public static final int NOTIFICATION_ID = 5453;
 
-    // This method runs on the main thread, so here we create the handler to show a message on the ui
-    @Override
-    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
-        mHandler = new Handler();
-        return super.onStartCommand(intent, flags, startId);
-    }
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -47,14 +52,44 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        // this post the toast to the main thread
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                // getApplicationContext because a service does't have access the screen
-                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-            }
-        });
-        Log.v("DelayedMessageService", "The message is: " +  text);
+
+        Log.i("Delayed mierda", text);
+
+        Intent actionIntent = new Intent(this, MainActivity.class);
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//        stackBuilder.addParentStack(MainActivity.class);
+//        stackBuilder.addNextIntent(intent);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                actionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        String notificationChannelId = "My channel_01";
+        int importantce = notificationManager.IMPORTANCE_HIGH;
+        NotificationChannel notificationChannel = new NotificationChannel(notificationChannelId, "human readabke tititle" importantce);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+//        Notification notification = new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(text)
+                .setChannel("my notification")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] {0, 1000})
+                .setAutoCancel(true)
+                .setContentIntent(actionPendingIntent);
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+
+//
+//        NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, notification);
+
+
+
     }
 }
