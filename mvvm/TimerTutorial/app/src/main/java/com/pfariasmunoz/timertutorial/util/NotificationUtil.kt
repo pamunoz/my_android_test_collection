7011800f62d589a651bd05dbf21cae684eac6dee
Pablo Munoz <pfariasmunoz@gmail.com>
2018-02-01 13:25:36 -0300

    Add function for creating a NotificationBuilder

--- mvvm/TimerTutorial/app/src/main/java/com/pfariasmunoz/timertutorial/util/NotificationUtil.kt
+++ mvvm/TimerTutorial/app/src/main/java/com/pfariasmunoz/timertutorial/util/NotificationUtil.kt
@@ -1,7 +1,13 @@
 package com.pfariasmunoz.timertutorial.util
 
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import android.support.v4.app.NotificationCompat
+import com.pfariasmunoz.timertutorial.AppConstants
+import com.pfariasmunoz.timertutorial.R
 import com.pfariasmunoz.timertutorial.TimerNotificationActionReceiver
 
 class NotificationUtil {
@@ -14,7 +20,23 @@ class NotificationUtil {
             // We want to be able to control the timer from notifications
             val startIntent = Intent(context, TimerNotificationActionReceiver::class.java)
             // specify actions for the intent
-            startIntent.action
+            startIntent.action = AppConstants.ACTION_START
+            val startPendingIntent = PendingIntent.getBroadcast(context, 0,
+                    startIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            // Create the actual notifications
+            val nBuilder = getBasicNotificationBuilder(context, CHANNEL_ID_TIMER, true)
+        }
+
+        private fun getBasicNotificationBuilder(
+                context: Context, channeLId: String, playSound: Boolean)
+                : NotificationCompat.Builder {
+            val notificationSound: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val nBuilder = NotificationCompat.Builder(context, channeLId)
+                    .setSmallIcon(R.drawable.ic_timer)
+                    .setAutoCancel(true)
+                    .setDefaults(0)
+            if (playSound) nBuilder.setSound(notificationSound)
+            return nBuilder
         }
 
     }
