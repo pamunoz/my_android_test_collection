068274cabd731ab1c215dcf97365ce6e0bf39b0f
Pablo Munoz <pfariasmunoz@gmail.com>
2018-02-01 12:01:07 -0300

    Add Notification Util class

---
+++ mvvm/TimerTutorial/app/src/main/java/com/pfariasmunoz/timertutorial/util/NotificationUtil.kt
@@ -0,0 +1,10 @@
+package com.pfariasmunoz.timertutorial.util
+
+class NotificationUtil {
+    companion object {
+        private const val CHANNEL_ID_TIMER = "menu_timer"
+        private const val CHANNEL_NAME_TIMER = "Timer App Timer"
+        private const val TIMER_ID = 0
+
+    }
+}
