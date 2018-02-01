d8c96183a9ffd5674e9d8c0d780e83c368d7ab95
Pablo Munoz <pfariasmunoz@gmail.com>
2018-02-01 13:12:06 -0300

    Add constant for the notification actions an a receiver

--- mvvm/TimerTutorial/app/src/main/java/com/pfariasmunoz/timertutorial/util/NotificationUtil.kt
+++ mvvm/TimerTutorial/app/src/main/java/com/pfariasmunoz/timertutorial/util/NotificationUtil.kt
@@ -1,10 +1,21 @@
 package com.pfariasmunoz.timertutorial.util
 
+import android.content.Context
+import android.content.Intent
+import com.pfariasmunoz.timertutorial.TimerNotificationActionReceiver
+
 class NotificationUtil {
     companion object {
         private const val CHANNEL_ID_TIMER = "menu_timer"
         private const val CHANNEL_NAME_TIMER = "Timer App Timer"
         private const val TIMER_ID = 0
+
+        fun showTimerExpired(context: Context) {
+            // We want to be able to control the timer from notifications
+            val startIntent = Intent(context, TimerNotificationActionReceiver::class.java)
+            // specify actions for the intent
+            startIntent.action
+        }
 
     }
 }
